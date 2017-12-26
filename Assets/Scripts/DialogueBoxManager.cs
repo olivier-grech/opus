53ce1ffa0e46f17727caf111c385c03af4350955
Olivier Grech <olivier.grech@gmail.com>
2017-12-26 15:26:25 +0100

    Add method to change name plate name

--- Assets/Scripts/DialogueBoxManager.cs
+++ Assets/Scripts/DialogueBoxManager.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class DialogueBoxManager : MonoBehaviour {
+public class DialogueBoxManager : MonoBehaviour
+{
+    public GameObject m_NamePlate;
+    private Text m_NamePlateName;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public GameObject m_TextBox;
+    private Text m_TextBoxText;
+
+    // Use this for initialization
+    void Start()
+    {
+		m_NamePlateName = m_NamePlate.GetComponentInChildren<Text>();
+		m_TextBoxText = m_TextBox.GetComponentInChildren<Text>();
+    }
+
+	void ChangeNamePlateName(string name)
+	{
+		m_NamePlateName.text = name;
 	}
 }
