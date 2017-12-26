6cbc641c0e3c40f5a09b628e6c80f517ef66d9cb
Olivier Grech <olivier.grech@gmail.com>
2017-12-26 14:44:35 +0100

    Create empty methods in scene manager and import backgrounds dictionnary

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour {
+
+	public static SceneManager instance;
+
+	public Image m_BackgroundSprite;
+
+	public Image m_CharacterSprite;
+
+	// Use this for initialization
+	void Awake () {
+		instance = this;
+	}
+
+	// Lookup a background sprite in the background sprites dictionnary and display it as the background
+	void changeBackgroundSprite(string spriteName) {
+
+	}
+
+	// Lookup a background sprite in the background sprites dictionnary and display it as the background
+	void changeCharacterSprite(string spriteName) {
+
+	}
+	
+
+}
